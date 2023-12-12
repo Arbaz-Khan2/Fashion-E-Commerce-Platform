0bfe74137de34c636c454ef2cac751b70c394846
Arbaz Khan <91343514+Arbaz-Khan2@users.noreply.github.com>
2023-12-12 21:18:30 +0500

    Add files via upload

--- FashionECommerce/FashionECommerce/settings.py
+++ FashionECommerce/FashionECommerce/settings.py
@@ -83,20 +83,26 @@ WSGI_APPLICATION = 'FashionECommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'fashionecommerce',
+#         'USER': 'root',
+#         'PASSWORD': 'Pos@4444',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'init_command': 'SET default_storage_engine=INNODB',
+#         },
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'fashionecommerce',
-        'USER': 'root',
-        'PASSWORD': 'Pos@4444',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': 'SET default_storage_engine=INNODB',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
